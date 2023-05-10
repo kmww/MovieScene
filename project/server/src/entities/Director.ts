b409f45bea4cc09712baa4642f29869c3f57d62c
kmww <kmw9708@gmail.com>
2023-05-10 23:34:40 +0900

    feat: director field resolver 구현

---
+++ project/server/src/entities/Director.ts
@@ -0,0 +1,10 @@
+import { Field, Int, ObjectType } from 'type-graphql';
+
+@ObjectType()
+export class Director {
+  @Field(() => Int)
+  id: number;
+
+  @Field(() => String)
+  name: string;
+}
--- project/server/src/resolvers/Film.ts
+++ project/server/src/resolvers/Film.ts
@@ -1,4 +1,5 @@
-import { Query, Resolver } from 'type-graphql';
+import { FieldResolver, Root, Query, Resolver } from 'type-graphql';
+import { Director } from '../entities/Director';
 import ghibliData from '../data/ghibli';
 import { Film } from '../entities/Film';
 
@@ -7,5 +8,12 @@ export class FilmResolver {
   @Query(() => [Film])
   films(): Film[] {
     return ghibliData.films;
+  }
+
+  @FieldResolver(() => Director)
+  director(@Root() parentFilm: Film): Director | undefined {
+    return ghibliData.directors.find(
+      (director) => director.id === parentFilm.director_id,
+    );
   }
 }
