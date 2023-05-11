7ead1cd391af8f6ee86a4aec1a4357cc14861ac5
kmww <kmw9708@gmail.com>
2023-05-12 01:18:07 +0900

    feat: films 커서 기반 페이지네이션 구현

--- project/server/src/resolvers/Film.ts
+++ project/server/src/resolvers/Film.ts
@@ -1,13 +1,58 @@
-import { FieldResolver, Root, Query, Resolver } from 'type-graphql';
+import {
+  FieldResolver,
+  Root,
+  Query,
+  Resolver,
+  ObjectType,
+  Field,
+  Int,
+  Arg,
+} from 'type-graphql';
 import { Director } from '../entities/Director';
 import ghibliData from '../data/ghibli';
 import { Film } from '../entities/Film';
 
+@ObjectType()
+class PaginatedFilms {
+  @Field(() => [Film])
+  films: Film[];
+
+  @Field(() => Int, { nullable: true })
+  cursor?: Film['id'] | null;
+}
+
 @Resolver(Film)
 export class FilmResolver {
-  @Query(() => [Film])
-  films(): Film[] {
-    return ghibliData.films;
+  @Query(() => PaginatedFilms)
+  films(
+    @Arg('limit', () => Int, { nullable: true, defaultValue: 6 })
+    limit: number,
+    @Arg('cursor', () => Int, { nullable: true, defaultValue: 1 })
+    cursor: Film['id'],
+  ): PaginatedFilms {
+    const realLimit = Math.min(6, limit);
+
+    if (!cursor) return { films: [] };
+
+    const cursorDataIndex = ghibliData.films.findIndex(
+      (film) => film.id === cursor,
+    );
+
+    if (cursorDataIndex === -1) return { films: [] };
+
+    const result = ghibliData.films.slice(
+      cursorDataIndex,
+      cursorDataIndex + realLimit,
+    );
+
+    const nextCursor = result[result.length - 1].id + 1;
+    const hasNext =
+      ghibliData.films.findIndex((film) => film.id === nextCursor) > -1;
+
+    return {
+      cursor: hasNext ? nextCursor : null,
+      films: result,
+    };
   }
 
   @FieldResolver(() => Director)
