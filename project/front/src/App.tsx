65a65ac50619cbe5db735a490a4aec928a5cb2de
kmww <kmw9708@gmail.com>
2023-05-12 17:37:25 +0900

    feat: 무한스크롤 적용

--- project/front/src/App.tsx
+++ project/front/src/App.tsx
@@ -2,10 +2,32 @@ import * as React from 'react';
 import { Box, ChakraProvider, theme, Text } from '@chakra-ui/react';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import FilmList from './components/film/FilmList';
+import { PaginatedFilms } from './generated/graphql';
 
 const apolloClient = new ApolloClient({
   uri: 'http://localhost:4000/graphql',
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          films: {
+            keyArgs: false,
+            merge: (
+              existing: PaginatedFilms | undefined,
+              incoming: PaginatedFilms,
+            ): PaginatedFilms => {
+              return {
+                cursor: incoming.cursor,
+                films: existing
+                  ? [...existing.films, ...incoming.films]
+                  : incoming.films,
+              };
+            },
+          },
+        },
+      },
+    },
+  }),
 });
 
 export const App = () => (
