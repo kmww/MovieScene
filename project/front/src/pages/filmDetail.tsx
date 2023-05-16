5f2bd1929a404376527f2623606c6a3779482cbf
kmww <kmw9708@gmail.com>
2023-05-16 22:24:40 +0900

    feat: filmDetail 정보 불러오기

--- project/front/src/pages/filmDetail.tsx
+++ project/front/src/pages/filmDetail.tsx
@@ -1,8 +1,25 @@
 import { ReactElement } from 'react';
 import CommonLayout from '../components/CommonLayout';
+import { useParams } from 'react-router-dom';
+import { useFilmQuery } from '../generated/graphql';
+import { Box, Spinner, Text } from '@chakra-ui/react';
 
 const FilmDetail = (): ReactElement => {
-  return <CommonLayout>영화 상세 정보 페이지</CommonLayout>;
+  const { filmId } = useParams();
+  const { data, loading, error } = useFilmQuery({
+    variables: { filmId: Number(filmId) },
+  });
+
+  return (
+    <CommonLayout>
+      {loading && <Spinner />}
+      {error && <Text>페이지를 표시할 수 없습니다.</Text>}
+
+      <Box>
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      </Box>
+    </CommonLayout>
+  );
 };
 
 export default FilmDetail;
