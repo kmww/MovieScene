bedb20ed68ad4784a4fcf51f41bb666cbd40dec8
kmww <kmw9708@gmail.com>
2023-05-16 22:49:52 +0900

    feat: 영화정보 컴포넌트 구현

---
+++ project/front/src/components/film/FilmInfomation.tsx
@@ -0,0 +1,49 @@
+import { ReactElement } from 'react';
+import { FilmQuery } from '../../generated/graphql';
+import { Box, Flex, Heading, Image, Tag, Text } from '@chakra-ui/react';
+
+interface FilmInfomationProps {
+  film: FilmQuery['film'];
+}
+
+const FilmInfomation = ({ film }: FilmInfomationProps): ReactElement => {
+  return (
+    <Flex
+      mt={12}
+      flexDirection={{ base: 'column', md: 'row' }}
+      alignItems="center"
+    >
+      <Box flex={1} maxW={250}>
+        <Image src={film?.posterImg} borderRadius={20} />
+      </Box>
+
+      <Flex
+        flex={1}
+        ml={{ base: 0, md: 6 }}
+        flexDirection="column"
+        alignContent="center"
+        justify="center"
+        alignItems="flex-start"
+      >
+        <Flex mt={2}>
+          {film?.genre.split(',').map((genre) => (
+            <Tag key={genre} mr={2} size="sm">
+              {genre}
+            </Tag>
+          ))}
+        </Flex>
+        <Heading mt={2} mb={4}>
+          {film?.title}
+          {film?.release ? `(${new Date(film.release).getFullYear()})` : null}
+        </Heading>
+        <Text mb={2}>
+          {film?.director.name}
+          {' • '} {film ? `${film.runningTime}분` : ''}
+        </Text>
+        <Text fontSize="sm">{film?.description}</Text>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default FilmInfomation;
--- project/front/src/pages/filmDetail.tsx
+++ project/front/src/pages/filmDetail.tsx
@@ -3,6 +3,9 @@ import CommonLayout from '../components/CommonLayout';
 import { useParams } from 'react-router-dom';
 import { useFilmQuery } from '../generated/graphql';
 import { Box, Spinner, Text } from '@chakra-ui/react';
+import FilmInfomation from '../components/film/FilmInfomation';
+
+const ERROR_MESSAGE = '페이지를 표시할 수 없습니다.';
 
 const FilmDetail = (): ReactElement => {
   const { filmId } = useParams();
@@ -13,10 +16,14 @@ const FilmDetail = (): ReactElement => {
   return (
     <CommonLayout>
       {loading && <Spinner />}
-      {error && <Text>페이지를 표시할 수 없습니다.</Text>}
+      {error && <Text>{ERROR_MESSAGE}</Text>}
 
       <Box>
-        <pre>{JSON.stringify(data, null, 2)}</pre>
+        {filmId && data?.film ? (
+          <FilmInfomation film={data.film} />
+        ) : (
+          <Text>{ERROR_MESSAGE}</Text>
+        )}
       </Box>
     </CommonLayout>
   );
