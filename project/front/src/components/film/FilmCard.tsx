c4837805506f0e7ab8a687c768a5d9c93816d16a
kmww <kmw9708@gmail.com>
2023-05-16 23:02:41 +0900

    feat: 상세페이지 라우터 연결

--- project/front/src/components/film/FilmCard.tsx
+++ project/front/src/components/film/FilmCard.tsx
@@ -4,11 +4,13 @@ import {
   Heading,
   Image,
   LinkBox,
+  LinkOverlay,
   Stack,
   Text,
   useColorModeValue,
 } from '@chakra-ui/react';
 import { FilmsQuery } from '../../generated/graphql';
+import { Link } from 'react-router-dom';
 
 interface FilmCardProps {
   film: FilmsQuery['films']['films'][0];
@@ -31,13 +33,15 @@ const FilmCard = ({ film }: FilmCardProps) => {
           </AspectRatio>
         </Box>
         <Stack>
-          <Heading
-            color={useColorModeValue('gray.700', 'white')}
-            fontSize="xl"
-            fontFamily="body"
-          >
-            {film.title}
-          </Heading>
+          <LinkOverlay as={Link} to={`/film/${film.id}`}>
+            <Heading
+              color={useColorModeValue('gray.700', 'white')}
+              fontSize="xl"
+              fontFamily="body"
+            >
+              {film.title}
+            </Heading>
+          </LinkOverlay>
           <Text fontSize="sm" color="gray.500" isTruncated>
             {film.subtitle ? film.subtitle : <>&nbsp;</>}
           </Text>
