1eb3a01ffba3440073ad879a385f35726f78daf5
kmww <kmw9708@gmail.com>
2023-05-17 20:09:41 +0900

    feat: filmcut lazyload 적용

--- project/front/src/components/film-cut/FilmCutList.tsx
+++ project/front/src/components/film-cut/FilmCutList.tsx
@@ -1,6 +1,7 @@
 import { ReactElement } from 'react';
 import { useCutsQuery } from '../../generated/graphql';
 import { SimpleGrid, Image, Box, Spinner } from '@chakra-ui/react';
+import LazyLoad from 'react-lazyload';
 
 interface FilmCutListProps {
   filmId: number;
@@ -20,7 +21,9 @@ const FilmCutList = ({ filmId }: FilmCutListProps): ReactElement => {
   return (
     <SimpleGrid my={4} columns={[1, null, 3]} spacing={[2, null, 8]}>
       {data?.cuts.map(({ src, id }) => (
-        <Image src={src} key={id} />
+        <LazyLoad height={200} once key={id}>
+          <Image src={src} />
+        </LazyLoad>
       ))}
     </SimpleGrid>
   );
