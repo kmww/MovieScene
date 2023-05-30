3f07031cf1a0e8cfa683af493e7c8d7ab5504186
kmww <kmw9708@gmail.com>
2023-05-30 17:07:56 +0900

    feat: apolloclient 인스턴스화

--- project/front/src/apollo/createApolloClient.ts
+++ project/front/src/apollo/createApolloClient.ts
@@ -1,9 +1,35 @@
-import { ApolloClient, NormalizedCacheObject } from '@apollo/client';
+import {
+  ApolloClient,
+  NormalizedCacheObject,
+  HttpLink,
+  from,
+} from '@apollo/client';
 import { createApolloCache } from './createApolloCache';
+import { onError } from '@apollo/client/link/error';
+
+const errorLink = onError(({ graphQLErrors, networkError, operation }) => {
+  if (graphQLErrors) {
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      console.log(`[GraphQL error]: -> ${operation.operationName}
+    Message: ${message}, Query: ${path}, Location: ${JSON.stringify(
+        locations,
+      )}`),
+    );
+  }
+
+  if (networkError) {
+    console.log(`[networkError]: -> ${operation.operationName}
+    Message: ${networkError.message}`);
+  }
+});
+
+const httpLink = new HttpLink({
+  uri: 'http://localhost:4000/graphql',
+});
 
 export const createApolloClient = (): ApolloClient<NormalizedCacheObject> => {
   return new ApolloClient({
-    uri: 'http://localhost:4000/graphql',
     cache: createApolloCache(),
+    link: from([errorLink, httpLink]),
   });
 };
