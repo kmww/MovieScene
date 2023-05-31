8ee3ea5e726b625d8988fd5570c99e08b1532a74
kmww <kmw9708@gmail.com>
2023-06-01 00:24:35 +0900

    feat: 리프레시 토큰 발급

--- project/server/src/utils/jwt-auth.ts
+++ project/server/src/utils/jwt-auth.ts
@@ -4,11 +4,13 @@ import { IncomingHttpHeaders } from 'http';
 import User from '../entities/User';
 
 export const DEFAULT_JWT_SECRET_KEY = 'secret-key';
+export const REFRESH_JWT_SECRET_KEY = 'secret-key2';
 
 export interface JwtVerifiedUser {
   userId: User['id'];
 }
 
+/* 엑세스 토큰 발급 */
 export const createAccessToken = (user: User): string => {
   const userData: JwtVerifiedUser = { userId: user.id };
   const accessToken = jwt.sign(
@@ -48,4 +50,14 @@ export const verifyAccessTokenFromReqHeaders = (
   } catch {
     return null;
   }
+};
+
+/* 리프레시 토큰 발급 */
+export const createRefreshToken = (user: User): string => {
+  const userData: JwtVerifiedUser = { userId: user.id };
+  return jwt.sign(
+    userData,
+    process.env.JWT_REFRESH_SECRET_KEY || REFRESH_JWT_SECRET_KEY,
+    { expiresIn: '14d' },
+  );
 };
