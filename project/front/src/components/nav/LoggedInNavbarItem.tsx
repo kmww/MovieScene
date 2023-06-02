3f37d1ac4f0b5db6be9153f08d749ff5fa41fd01
kmww <kmw9708@gmail.com>
2023-06-02 17:20:23 +0900

    feat: 로그아웃 기능

--- project/front/src/components/nav/LoggedInNavbarItem.tsx
+++ project/front/src/components/nav/LoggedInNavbarItem.tsx
@@ -1,12 +1,45 @@
-import { Avatar, Stack } from '@chakra-ui/react';
+import {
+  Avatar,
+  Button,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Stack,
+} from '@chakra-ui/react';
 import { ReactElement } from 'react';
 import { ColorModeSwitcher } from '../ColorModeSwitcher';
+import { useApolloClient } from '@apollo/client';
+import { useLogoutMutation } from '../../generated/graphql';
 
 const LoggedInNavbarItem = (): ReactElement => {
+  const client = useApolloClient();
+  const [logout, { loading: logoutLoading }] = useLogoutMutation();
+
+  const onLogoutClick = async () => {
+    try {
+      await logout();
+      localStorage.removeItem('access_token');
+      await client.resetStore();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <Stack justify="flex-end" alignItems="center" direction="row" spacing={3}>
       <ColorModeSwitcher />
-      <Avatar size="sm" />
+
+      <Menu>
+        <MenuButton as={Button} rounded="fill" variant="link" cursor="pointer">
+          <Avatar size="sm" />
+        </MenuButton>
+        <MenuList>
+          <MenuItem isDisabled={logoutLoading} onClick={onLogoutClick}>
+            로그아웃
+          </MenuItem>
+        </MenuList>
+      </Menu>
     </Stack>
   );
 };
