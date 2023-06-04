ff34770580a169b6dab9c42350c1c39a69bf2c14
kmww <kmw9708@gmail.com>
2023-06-04 16:13:56 +0900

    feat: 좋아요 버튼 구현

--- project/front/src/components/film-cut/FilmCutInfo.tsx
+++ project/front/src/components/film-cut/FilmCutInfo.tsx
@@ -2,19 +2,35 @@ import {
   AspectRatio,
   Box,
   Button,
+  Text,
   Flex,
   HStack,
   Heading,
   Image,
+  useColorModeValue,
 } from '@chakra-ui/react';
+import { ReactElement } from 'react';
 import { FaHeart } from 'react-icons/fa';
+import { useVoteMutation } from '../../generated/graphql';
 
 interface FilmCutInfoProps {
   cutImg: string;
   cutId: number;
+  isVoted?: boolean;
+  voteCount?: number;
 }
 
-const FilmCutInfo = ({ cutImg, cutId }: FilmCutInfoProps) => {
+const FilmCutInfo = ({
+  cutImg,
+  cutId,
+  isVoted = false,
+  voteCount = 0,
+}: FilmCutInfoProps): ReactElement => {
+  const voteButtonColor = useColorModeValue('gray.500', 'gray.400');
+  const [vote, { loading: voteLoading }] = useVoteMutation({
+    variables: { cutId },
+  });
+
   return (
     <Box>
       <AspectRatio ratio={16 / 9}>
@@ -25,7 +41,15 @@ const FilmCutInfo = ({ cutImg, cutId }: FilmCutInfoProps) => {
         <Flex justify="space-between" alignItems="center">
           <Heading size="sm">{cutId}번째 사진</Heading>
           <HStack spacing={1} alignItems="center">
-            <Button aria-label="like-this-cut-button" leftIcon={<FaHeart />} />
+            <Button
+              aria-label="like-this-cut-button"
+              color={isVoted ? 'pink.400' : voteButtonColor}
+              leftIcon={<FaHeart />}
+              isLoading={voteLoading}
+              onClick={() => vote()}
+            >
+              <Text>{voteCount}</Text>
+            </Button>
             <Button colorScheme="teal">리뷰 남기기</Button>
           </HStack>
         </Flex>
