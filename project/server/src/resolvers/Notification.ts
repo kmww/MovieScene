d4ca8ed19d2c777acd2e6a61f1e7d0809de85b83
kmww <kmw9708@gmail.com>
2023-06-23 17:27:36 +0900

    feat: 알림 리졸버

---
+++ project/server/src/resolvers/Notification.ts
@@ -0,0 +1,21 @@
+import { Ctx, Query, Resolver, UseMiddleware } from 'type-graphql';
+import { MyContext } from '../apollo/createApolloServer';
+import Notification from '../entities/Notification';
+import { isAuthenticated } from '../middlewares/isAuthenticated';
+
+@Resolver(Notification)
+export class NotificationResolver {
+  @Query(() => [Notification], {
+    description: '세션에 해당되는 유저의 모든 알림을 가져옵니다.',
+  })
+  @UseMiddleware(isAuthenticated)
+  async notifications(
+    @Ctx() { verifiedUser }: MyContext,
+  ): Promise<Notification[]> {
+    const notifications = await Notification.find({
+      where: { userId: verifiedUser.userId },
+      order: { createdAt: 'DESC' },
+    });
+    return notifications;
+  }
+}
